3a55b101a520ba26c813f3ceaa971c67ac5aefcf
Gabriel Hernandes <gabriel.hernandess01@gmail.com>
2025-09-27 12:01:12 -0300

    feat:
    - Criado os models Category e Recipe
    
    - Criado função Faker para teste de frontend
    - Atualizado recipe e recipe_view, com seus respectivos css
    
    - Inserido app_name em urls.py
    - Atualizado todos os redirecionamentos href dos htmls para url do django via name
    - Criado blocos para títulos de página

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Yanagawa'
+        'recipes': [make_recipe() for _ in range(10)]
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'id': id
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
