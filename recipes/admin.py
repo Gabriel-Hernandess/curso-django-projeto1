65b75777c9c4fee1cb9573a18c585e6ce2759f87
Gabriel Hernandes <gabriel.hernandess01@gmail.com>
2025-10-06 19:12:24 -0300

    - Implementado os models no Django Admin e configurado o urls.py para identificar os statics e medias.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Recipe, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
